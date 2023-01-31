b08535f2b40f5c6c02be1629f51bd5e9f617564f
Sukanya <sukanyapamuluru4545@gmail.com>
2023-01-31 13:56:09 +0530

    sample commit

---
+++ sampleforgit/src/main/java/com/sample/app/SampleforgitApplication.java
@@ -0,0 +1,13 @@
+package com.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleforgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleforgitApplication.class, args);
+	}
+
+}
